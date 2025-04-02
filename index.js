e4cba24320a42b1626e0dde39818a944da6a517c
eaa24martia <eaa24martia@gmail.com>
2025-04-02 11:11:03 +0200

    Made sure that the dodger can go both left and right

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+"use strict";
+
+const dodger = document.getElementById("dodger");
+
+
+// Funktion, der flytter dodgeren til venstre
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// Funktion, der flytter dodgeren til højre
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10); 
+
+  if (left < 360) { 
+    dodger.style.left = `${left + 1}px`; 
+  }
+}
+
+// Listener, der tjekker for om der bliver trykket venstre eller højre
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight(); 
+  }
+});
